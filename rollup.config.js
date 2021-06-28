f881b6a36dda7604427d06b02f654b0404abb5f6
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2021-06-28 13:39:03 -0400

    First commit

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+module.exports = [{
+  input: "autocomplete-input.js",
+  output: {
+    file: "autocomplete-input.umd.js",
+    format: "umd",
+    name: "NPRAutocompleteInput",
+    compact: true
+  }
+}];
